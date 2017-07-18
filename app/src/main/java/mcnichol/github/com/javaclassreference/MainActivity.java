934d8a5ac396f95fc2ecf913138eec62ff4c4d1d
McNichol <mmcnichol@gogoair.com>
2017-07-18 07:29:47 -0500

    New project setup with Empty Activity

---
+++ app/src/main/java/mcnichol/github/com/javaclassreference/MainActivity.java
@@ -0,0 +1,13 @@
+package mcnichol.github.com.javaclassreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
